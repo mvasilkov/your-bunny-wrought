1eef1f111e0c035b325b7f3b83c94c79bee8a7d1
Mark Vasilkov <mvasilkov@gmail.com>
2023-11-06 12:21:54 +0200

    Tools

--- yourbunnywrought/external/__init__.py
+++ yourbunnywrought/external/__init__.py
@@ -1,3 +1,16 @@
+from enum import Enum
+
+from ..lazy import LazyVariable
 from .external_executable import ExternalExecutable
 
-__all__ = ['ExternalExecutable']
+__all__ = ['ExternalExecutable', 'Tools']
+
+
+class Tools(Enum):
+    ffmpeg = LazyVariable(
+        lambda: ExternalExecutable(
+            executable='ffmpeg',
+            version_option='-version',
+            version_pattern=r'ffmpeg version (\S+)',
+        )
+    )
