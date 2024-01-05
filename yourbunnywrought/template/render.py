0972d72ba2ed2adfa80857465e7e4c85c84e32be
Mark Vasilkov <mvasilkov@gmail.com>
2024-01-06 01:31:02 +0200

    Update

--- yourbunnywrought/template/render.py
+++ yourbunnywrought/template/render.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from functools import cache
 from pathlib import Path
+from typing import Iterable
 
 from django.template import Context, Engine
 
@@ -11,16 +12,19 @@ __all__ = ['init_cli', 'invoke_cli', 'render_to_string']
 
 
 @cache
-def get_engine(dirs: list[str] | None) -> Engine:
+def get_engine(dirs: tuple[str] | None) -> Engine:
     return Engine(dirs=dirs)
 
 
-def render_to_string(template_str: str, context: dict, dirs: list[str] | None = None) -> str:
-    template = get_engine(dirs).from_string(template_str)
+def render_to_string(template_str: str, context: dict, dirs: Iterable[str] | None = None) -> str:
+    template = get_engine(tuple(dirs)).from_string(template_str)
     return template.render(Context(context))
 
 
-def render_template(infile: Path, context: dict, dirs: list[str] | None = None):
+def render_template(infile: Path, context: dict, dirs: Iterable[str] | None = None):
+    if dirs is not None:
+        dirs = tuple(dirs)
+
     outfile = infile.with_name(infile.name[1:])
     outfile.write_text(
         render_to_string(infile.read_text(encoding='utf-8'), context, dirs),
@@ -50,4 +54,5 @@ def invoke_cli(args):
         case 'render_template' | 'template':
             for pattern in args.pattern:
                 for path in store.working_directory.glob(pattern):
+                    print('*', path.relative_to(store.working_directory))
                     render_template(path, {}, args.template_dir)
